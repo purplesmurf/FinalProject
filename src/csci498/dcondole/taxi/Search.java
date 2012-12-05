db01fdd096556ee262c6a679913abe2c10936622
Dominick Condoleo <dcondole@mines.edu>
2012-12-05 12:06:34 -0700

    does a basic google search to summon the list of taxis in the area

--- src/csci498/dcondole/taxi/Search.java
+++ src/csci498/dcondole/taxi/Search.java
@@ -5,14 +5,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class Search extends Activity {
+	private WebView browser;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.taxi);
+        
+        browser = (WebView)findViewById(R.id.webkit);
+        browser.loadUrl("http://www.google.com/search?q=taxi");
     }
 
 }
