2cc490709291af5a211dfcae61459179b6ef6092
Dominick Condoleo <dcondole@mines.edu>
2012-12-10 10:48:07 -0700

    Added a toast for when no internet access

--- src/csci498/dcondole/taxi/Search.java
+++ src/csci498/dcondole/taxi/Search.java
@@ -1,23 +1,42 @@
 package csci498.dcondole.taxi;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Search extends Activity {
 	private WebView browser;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.taxi);
-        
-        browser = (WebView)findViewById(R.id.webkit);
-        browser.loadUrl("http://www.google.com/search?q=taxi");
-    }
+	private static final String url = "http://www.google.com/search?q=taxi";
+	private String noInternetMessage = "The Internet is not currently available please try again later but don't go driving";
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.taxi);
+
+		checkAndSearch();
+	}
+
+	public void checkAndSearch()
+	{
+		ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED || 
+				connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+			//we are connected to a network
+			browser = (WebView)findViewById(R.id.webkit);
+			browser.loadUrl(url);
+		}
+		else {
+			Toast.makeText(this, noInternetMessage, Toast.LENGTH_LONG).show();
+		}
+	}
 }
